c36ee25975c6a587b6fc96cdf3ae732e0270e189
abhihacker02 <abhi04.sati@gmail.com>
2017-06-19 14:41:10 +0530

    Create froy.py
    The Source Code

---
+++ froy.py
@@ -0,0 +1,86 @@
+impo11rt os
+import sys
+import ctypes
+import random
+import win32com.client as wincl     #pip install pypiwin32
+import speech_recognition as sr     #pip install speechrecognition
+import webbrowser                   #pip install webbrowser
+from bs4 import BeautifulSoup       #pip install beautifullsoup/Beautifullsoup
+import requests                     #pip install requests
+import winshell                     #pip install winshell
+from urllib.request import urlopen  #pip install urllib
+requests.packages.urllib3.disable_warnings()
+leave=0
+videos=['Ed Sheeran - Shape of You [Official Video].mp4','Ellie Goulding - Burn.mp4'
+        ,'Agar Tum Saath Ho  FULL VIDEO SONG  Tamasha  Ranbir Kapoor & Deepika Padukone.mp4'
+        ,'Afreen Afreen, Rahat Fateh Ali Khan & Momina Mustehsan, Episode 2, Coke Studio Season 9.mp4'
+        ,'bulleya.mp4','love me like u do.mp4','boulevards of broken dreams.mp4','numb.mp4','what makes u beautifull.mp4']
+speak = wincl.Dispatch("SAPI.SpVoice")
+while(leave==0):
+    r = sr.Recognizer()
+    speak.Speak("sir,speak what should i do for you")
+    with sr.Microphone() as source:
+#for mic use sr.Microphone() as source and r.listen instead of r.record
+#print("Say something! I'll try to recognize it")   use sr.AudioFile("woman1_wb.wav") as source for reading audio file and r.record instead of r.listen
+        r.adjust_for_ambient_noise(source, duration = 0.5)  
+        audio = r.listen(source)
+    try:
+        put=r.recognize_google(audio, key = None, language = "en-US", show_all = False)
+        print("You said:" + put)
+    except sr.UnknownValueError:
+        again=1
+        print("I tried hard but could not understand what you spoke")
+    except sr.RequestError as e:
+        again=1
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    except:
+        again=1
+        print("Unknown exception occurred!")
+    if(again==1):
+        again=0
+        continue
+    put=put.lower()
+    link=put.split()
+    if(put.startswith('open')):
+        try:
+            webbrowser.open('http://www.'+link[1]+'.com')
+        except:
+            print("sorry,no internet connection")
+    elif(put.startswith('play')):
+        try:
+            link = '+'.join(link[1:])
+            say = link.replace('+', ' ')
+            url = 'https://www.youtube.com/results?search_query='+link
+            source_code = requests.get(url, headers=headers,timeout=50)
+            plain_text = source_code.text
+            soup = BeautifulSoup(plain_text, "html.parser")
+            songs = soup.findAll('div', {'class': 'yt-lockup-video'})
+            song = songs[0].contents[0].contents[0].contents[0]
+            hit = song['href']
+            speak.Speak("playing "+say)
+            webbrowser.open('https://www.youtube.com'+hit)
+        except:
+            print('Sorry, No internet connection!')
+    elif put.startswith('empty '):
+        try:
+            winshell.recycle_bin().empty(confirm=False,show_progress=False, sound=True)
+            print("Recycle Bin Empty!!")
+        except:
+            print("Unknown Error")
+    elif put.startswith('lock'):
+        try:
+            speak.Speak("locking the device")
+            ctypes.windll.user32.LockWorkStation()
+        except Exception as e:
+            print(str(e))
+    elif put.endswith('bored'):
+        try:
+            speak.Speak('''Sir, I\'m playing a dance video.Hope you like it''')
+            song = random.choice(videos)
+            os.startfile(song)
+        except Exception as e:
+            print(str(e))
+    elif(put.endswith('program')):
+         leave=1
+speak.Speak("closing the program")
+quit
